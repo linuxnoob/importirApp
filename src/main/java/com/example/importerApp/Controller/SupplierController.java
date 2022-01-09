f7e7be2b2520594ceeec4d4c45ae4823321b1854
gburnama <blackprimenoz@gmail.com>
2022-01-09 13:38:10 +0700

    CRUD supplier

--- src/main/java/com/example/importerApp/Controller/SupplierController.java
+++ src/main/java/com/example/importerApp/Controller/SupplierController.java
@@ -4,6 +4,8 @@ import com.example.importerApp.Entity.Supplier;
 import com.example.importerApp.Service.SupplierService;
 import org.apache.catalina.LifecycleState;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,4 +26,50 @@ public class SupplierController {
     public Supplier getSupplierById(@RequestParam("supplierId") Integer supplierId){
         return this.supplierService.getSupplierById(supplierId);
     }
+
+    @RequestMapping(path = "/saveSupplier", method = RequestMethod.POST, produces ={"application/json"})
+    public ResponseEntity<String> saveSupplier(@RequestBody Supplier supplier){
+        Supplier supplier1= null;
+        try {
+            supplier1 = this.supplierService.saveSupplier(supplier);
+            if(supplier1!=null){
+                return new ResponseEntity<>("save success "+ supplier, HttpStatus.CREATED);
+            }
+        }catch (Exception e){
+            System.out.println("error " + e.getMessage());
+        }
+        return new ResponseEntity<>("save error", HttpStatus.EXPECTATION_FAILED);
+    }
+
+    @RequestMapping(path = "/updateBySupplier", method = RequestMethod.PUT, produces ={"application/json"})
+    public ResponseEntity<String> updateSupplier(@RequestBody Supplier supplier){
+
+        try{
+            Supplier supplier1 = this.supplierService.updateSupplier(supplier);
+            if(supplier == null){
+                return new ResponseEntity<>("update error " , HttpStatus.BAD_REQUEST);
+            }
+
+        }catch (Exception e){
+            System.out.println("Error " + e.getMessage());
+        }
+        return new ResponseEntity<>("save successs ", HttpStatus.OK);
+    }
+    @RequestMapping(path = "/updateSupplierById", method = RequestMethod.DELETE, produces ={"application/json"})
+    public ResponseEntity<String> deleteSupplier(@RequestParam("supplierId") Integer supplierId){
+
+        try{
+           Integer status = this.supplierService.deleteSuppById(supplierId);
+            if(status == null){
+                return new ResponseEntity<>("update error " ,HttpStatus.BAD_REQUEST);
+            }
+
+        }catch (Exception e){
+            System.out.println("Error " + e.getMessage());
+        }
+        return new ResponseEntity<>("save successs ", HttpStatus.OK);
+    }
+
+
+
 }
--- src/main/java/com/example/importerApp/Service/SupplierService.java
+++ src/main/java/com/example/importerApp/Service/SupplierService.java
@@ -1,12 +1,17 @@
 package com.example.importerApp.Service;
 
+import com.example.importerApp.Entity.Customers;
 import com.example.importerApp.Entity.Supplier;
 import com.example.importerApp.Repo.SupplierRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +27,47 @@ public class SupplierService {
     public Supplier getSupplierById(Integer supplierId){
         return this.supplierRepo.findSupplierById(supplierId);
 
+    }
+
+    public Supplier saveSupplier(Supplier supplier) throws Exception{
+        try {
+            this.supplierRepo.save(supplier);
+            }catch (Exception e){
+            System.out.println("msg error " + e.getMessage());
+            return null;
+        }
+        return supplier;
+    }
+
+    public Supplier updateSupplier(Supplier supplier) throws Exception{
+        Supplier supp=null;
+        try {
+            supp = this.getSupplierById(supplier.getSupplierId());
+                if (supp!=null){
+                    this.supplierRepo.save(supplier);
+                }else {
+                    return null;
+                }
+
+        }catch (Exception e){
+            System.out.println("error msg " + e.getMessage());
+        }
+        return supplier;
+    }
+
+    public Integer deleteSuppById(Integer supplierId)throws Exception{
+        try {
+            Supplier supplier = this.getSupplierById(supplierId);
+                if (supplier !=null){
+                    this.supplierRepo.deleteById(supplierId);
+                } else {
+                    return null;
+                }
+
+        }catch (Exception e){
+            System.out.println(" error msg " + e.getMessage());
+        }
+        return supplierId;
     }
 
 }
